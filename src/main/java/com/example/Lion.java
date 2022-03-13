4353240ddc1b16c5a9d4a48331a065d3fe6489e4
Ql1ke <ql1ke@yandex.ru>
2022-03-13 15:47:03 +0300

    Lion с DI для класса в комментах

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -20,11 +20,41 @@ public class Lion {
 //     47-я минута вебинара от 10.03
 
 /*
+    DI через метод Set (сеттер)
     private Predator predator;
     public void setPredator(Predator predator) {
         this.predator = predator;
     }
+
+    DI через конструктор
+    private Predator predator;
+    public Lion(Predator predator) {
+        this.predator = predator;
+    }
 */
+
+
+/*
+* НО Class Feline наследет не интерфейс Predator, а класс Animal. Поэтому надо создать переменную с типом класса
+* Animal и брать метод getFood уже из класса Animal.
+*
+* private Animal food;
+* public Lion(Animal food) {
+*   this.animal = food;
+* }
+
+/*
+     private Animal food;
+     public Lion(Animal food) {
+        this.food = food;
+     }
+
+     public List<String> getFood() throws Exception {
+         return food.getFood("Хищник");
+     }
+*/
+
+//    private Feline feline;
 
     public int getKittens() {
         return feline.getKittens();
